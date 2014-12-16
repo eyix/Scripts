c5b7c8dc65b7effa010542c77087d4666ecb1c27
Brian Rosen <brian.a.rosen@gmail.com>
2014-12-15 18:33:24 -0800

    Trying out git.

---
+++ dirtomp4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+date > log.txt
+for file in `ls|grep '\.mkv\|\.avi\|\.wmv\|\.flv'`
+do
+echo "$file">>log.txt
+#Video only
+#avconv -i "$file" -c:v libx264 -c:a copy "${file%%.*}.mp4"
+#Audio Only
+#avconv -i "$file" -c:v copy -c:a libmp3lame "${file%%.*}.mp4"
+#Audio and Video force
+avconv -y -i "$file" -c:v libx264 -c:a libvo_aacenc `echo $file | sed 's/...$/mp4/'`
+#mv "$file" OLD
+done
+date >> log.txt
+cat log.txt | mail -s "`pwd`" brian.a.rosen@gmail.com
+
